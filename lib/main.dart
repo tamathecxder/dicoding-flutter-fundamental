35a6104a8260d6f6ef46754e38f98f7c8f4d289a
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-09-16 12:57:42 +0700

    chore: making an example of stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -44,3 +44,35 @@ class Heading extends StatelessWidget {
     );
   }
 }
+
+class BiggerText extends StatefulWidget {
+  final String text;
+ 
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+ 
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+ 
+ 
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
